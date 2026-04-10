89ccd4e41f7d159a0cf67248f743f08ac19a3eed
Iivari van Uden <iivari2006@outlook.com>
2026-04-10 13:54:23 +0300

    add config for sprite single mode default

---
+++ Assets/Scripts/UnityConfig/SpriteSingleModeImporter.cs
@@ -0,0 +1,19 @@
+#if UNITY_EDITOR
+using UnityEditor;
+
+public class SpriteSingleModeImporter : AssetPostprocessor
+{
+    const int PostProcessOrder = 0;
+    public override int GetPostprocessOrder() => PostProcessOrder;
+
+    void OnPreprocessTexture()
+    {
+        var importer = assetImporter as TextureImporter;
+
+        if (importer.importSettingsMissing is false)
+            return;
+
+        importer.spriteImportMode = SpriteImportMode.Single;
+    }
+}
+#endif
